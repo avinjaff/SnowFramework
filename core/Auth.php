03d7304a00c49096409207274e156e010e93a509
tayyebi <tayyebimohammadreza@gmail.com>
2018-01-30 17:26:00 +0330

    Login

--- core/Auth.php
+++ core/Auth.php
@@ -3,9 +3,25 @@
 class Auth {
  
     public function IsValid($Username, $Password) {
-        // Your logic here
-        return true;
-        return false;
+		// TODO: Your logic here
+
+		/*
+		$query  = "SELECT " 
+		. "Id"
+		. " FROM `" . "Users"
+        . "` WHERE `PrimaryNumber`='" . $Username . "'"
+        . " AND `Password`='" . $Password . "';"; // RULE: Every table should contain a field called Id which is a PK, or we can define PK later.
+		$db = new Db();
+		$conn = $db->Open();
+		$result = mysqli_query($conn, $query);
+		if (!$result)
+			return false;
+		$num = mysqli_num_rows($result);
+		if ($num === 1)
+		 return true;
+		else
+		*/
+		return false;
     }
 
 }
