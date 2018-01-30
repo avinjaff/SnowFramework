7e6016480f64236ba80ca49a4027dc2c6c54574e
tayyebi <tayyebimohammadreza@gmail.com>
2018-01-30 17:34:48 +0330

    Auth to parent

--- controller/userController.php
+++ controller/userController.php
@@ -4,10 +4,10 @@ include('../model/User.php');
 
 class userController extends AController{
 
-	function GET($Auth = false){ 
-		parent::GET();
+	function GET($Auth = true){
+		parent::GET($Auth);
 		$model = new User();
-		$id =parent::getRequest()["id"];
+		$id = parent::getRequest("id");
 		$model->SetValue("Id", $id);
 		$model->Select();
 		$data = $model->GetProperties();
@@ -16,10 +16,10 @@ class userController extends AController{
 	}
 
 	function POST($Auth = false){ 
-		parent::POST();
-		$user = new User();
+		parent::POST($Auth);
+		$user = new User();	
 		foreach($user->GetProperties() as $key => $value){
-			$user->SetValue($key, parent::getRequest()[$key]);
+			$user->SetValue($key, parent::getRequest($key));
 		}
 		$user->Insert();
 		parent::setData($user->GetProperties());
@@ -28,20 +28,20 @@ class userController extends AController{
 
 	function PUT($Auth = false)
 	{
-		parent::PUT();
+		parent::PUT($Auth);
 		$user = new User();
 		foreach($user->GetProperties() as $key => $value){
-			$user->SetValue($key, parent::getRequest()[$key]);
+			$user->SetValue($key, parent::getRequest($key));
 		}
-		$user->Update(parent::getRequest()["previousId"]);
+		$user->Update(parent::getRequest("previousId"));
 		parent::setData($user->GetProperties());
 		parent::returnData();
 	}
 
-	function DELETE($Auth = false){ 
-		parent::DELETE();
+	function DELETE($Auth = false){
+		parent::DELETE($Auth);
 		$user = new User();
-		$user->SetValue("Id", parent::getRequest()["Id"]);
+		$user->SetValue("Id", parent::getRequest("Id"));
 		$user->Delete();
 		parent::setData($user->GetProperties());
 		parent::returnData();
