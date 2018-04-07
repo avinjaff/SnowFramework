d9646f7e0b2f42f7beded8c9b4853d6f8317211b
tayyebi <tayyebimohammadreza@gmail.com>
2018-04-08 00:44:07 +0430

    global error handling

--- web-client/js/jquery.tayyebi.js
+++ web-client/js/jquery.tayyebi.js
@@ -1,9 +1,24 @@
+
+/*
+
+Regarding to:
+
+https://stackoverflow.com/questions/2008052/
+
+*/
+
+$.ajaxSetup({
+  contentType: "application/x-www-form-urlencoded; charset=UTF-8"
+});
+
+
+
 /*
 
 
 Regarding to:
 
-https://stackoverflow.com/questions/2153917/how-to-send-a-put-delete-request-in-jquery
+https://stackoverflow.com/questions/2153917/
 
 And also:
 
@@ -28,6 +43,7 @@ jQuery.each( [ "put", "delete" ], function( i, method ) {
         data = undefined;
       }
       return jQuery.ajax({
+        contentType:"application/json; charset=utf-8",
         url: url,
         type: method,
         dataType: type,
@@ -36,3 +52,22 @@ jQuery.each( [ "put", "delete" ], function( i, method ) {
       });
     };
   });
+
+
+/*
+
+Regarding to:
+
+https://stackoverflow.com/questions/951791/
+
+
+*/
+
+
+
+window.onerror = function(msg, url, line, col, error) {
+  var extra = !col ? '' : '\ncolumn: ' + col;
+  extra += !error ? '' : '\nerror: ' + error;
+  // TODO: Disable in release
+  // alert(msg + "\nurl: " + url + "\nline: " + line + extra);
+};
