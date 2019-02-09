c55f43ada08faeba967ef80d6f35ba265e12732c
tayyebi <tayyebimohammadreza@gmail.com>
2019-02-09 10:50:05 +0330

    pruning...

--- public/plug-in/Links.php
+++ public/plug-in/Links.php
@@ -5,7 +5,9 @@ class Links
     public static function GenerateCssLinks($URL, $CURRENTLANGUAGE, $BASEURL)
     {
         $items = explode('/',preg_replace("/[^a-zA-Z0-9_\-\/اآبپتثجچحخدذرزسشصضطظعغفقکگلمنوهی]/","-",$URL));
-        $output = '';
+        $output = '
+<link rel="stylesheet" href="' . $BASEURL . 'public/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+';
         for ($i = 1 ; $i < count($items); $i++ )
         {
             $folders = '';
@@ -19,7 +21,12 @@ class Links
     public static function GenerateJsLinks($URL, $CURRENTLANGUAGE, $BASEURL)
     {
         $items = explode('/',preg_replace("/[^a-zA-Z0-9_\-\/اآبپتثجچحخدذرزسشصضطظعغفقکگلمنوهی]/","-",$URL));
-        $output = '';
+        $output = '
+<script src="' . $BASEURL . 'public/js/jquery.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="' . $BASEURL . 'public/js/jquery.tayyebi.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="' . $BASEURL . 'public/js/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+<script src="' . $BASEURL . 'public/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+';
         for ($i = 1 ; $i < count($items); $i++ )
         {
             $folders = '';
