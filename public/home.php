5cf02dac4d75bf260f660d01e0632219d4a98420
tayyebi <tayyebimohammadreza@gmail.com>
2019-02-09 13:44:49 +0330

    Home links

--- public/home.php
+++ public/home.php
@@ -8,10 +8,10 @@
         <a class="blog-header-logo text-dark" href="#"><?php echo $Translate::Label(Config::TITLE); ?></a>
       </div>
       <div class="col-4 d-flex justify-content-end align-items-center">
-        <a class="text-muted" href="#">
+        <a class="text-muted" href="<?php echo $BASEURL . 'explore' ?>">
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mx-3" focusable="false" role="img"><title>Search</title><circle cx="10.5" cy="10.5" r="7.5"></circle><line x1="21" y1="21" x2="15.8" y2="15.8"></line></svg>
         </a>
-        <a class="btn btn-sm btn-outline-secondary" href="#">Sign up</a>
+        <a class="btn btn-sm btn-outline-secondary" href="<?php echo $BASEURL . 'login' ?>"><?php echo $Translate::Label('ورود به سیستم'); ?></a>
       </div>
     </div>
   </header>
@@ -129,7 +129,7 @@
         <ol class="list-unstyled">
           <?php
           foreach (explode(',', Config::META_KEYWORDS) as $Keyword) {
-              echo '<li><a rel="search" href="' . $BASEURL . 'search/' . $Keyword . '"> ' . $Keyword . ' </a>' . '</li>';
+              echo '<li><a rel="search" href="' . $BASEURL . 'explore/' . $Keyword . '"> ' . $Keyword . ' </a>' . '</li>';
           }
           ?>
         </ol>
